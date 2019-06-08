bb27d8d618c63f581a516216bb89f36b693479ad
Brendan T White <brendan@thespia.com>
2019-06-08 21:31:26 +0930

    Basic model relationships

--- app/State.php
+++ app/State.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+	
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = ['term'];
+
+
+
+
+
+
+    /**
+     * Get the terms for the state.
+     */
+    public function terms()
+    {
+        return $this->hasMany('App\Term');
+    }
+
 }
--- app/Term.php
+++ app/Term.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Term extends Model
 {
-    //
+
+
+
+
+
+
+
+
+
+    /**
+     * Get the state that owns the term.
+     */
+    public function state()
+    {
+        return $this->belongsTo('App\State');
+    }
+
 }
