4df5061164e472a4a9c6cbf9c7172e9569c84b21
Brendan T White <brendan@thespia.com>
2019-06-18 15:14:47 +0930

    housekeeping

--- database/migrations/2019_06_18_054244_insert_terms_2020.php
+++ database/migrations/2019_06_18_054244_insert_terms_2020.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
+
+// To create next year's migration:
+// php artisan make:migration insert_terms_2021 --table=terms
+
+// After inserting terms, go to /weeks/generate in a browswer to generate weeks
 
 class InsertTerms2020 extends Migration
 {
@@ -26,7 +32,7 @@ class InsertTerms2020 extends Migration
     public function down()
     {
         Schema::table('terms', function (Blueprint $table) {
-            //
+            // no trivial way to delete just these terms
         });
     }
 }
