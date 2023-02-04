23be7e007573996790b3e5a01d0bfcd56e3f9160
Brendan T White <brendan@thespia.com>
2023-02-04 12:08:27 +1030

    boilerplate

---
+++ database/migrations/2023_02_04_000023_insert_terms_2023.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
+
+// To create next year's migration:
+// php artisan make:migration insert_terms_2023 --table=terms
+
+// After inserting terms, go to /weeks/generate in a browswer to generate weeks
+
+class InsertTerms2023 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('terms', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('terms', function (Blueprint $table) {
+            //
+        });
+    }
+}
